8e0ba0d6000c1dc5a9357414bb786ada038bcb6f
Breno Arouca <brennoarouca@gmail.com>
2025-04-07 14:13:55 -0300

    configuração imports mongoose e configModule para leitura das váriaveis ambiente

--- nest-api/src/app.module.ts
+++ nest-api/src/app.module.ts
@@ -2,9 +2,17 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { RoutesModule } from './routes/routes.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [RoutesModule],
+  imports: [
+    ConfigModule.forRoot(),
+    RoutesModule,
+    MongooseModule.forRoot(
+      process.env.MONGO_DSN || 'mongodb://root:root@db/nest?authSource=admin',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
